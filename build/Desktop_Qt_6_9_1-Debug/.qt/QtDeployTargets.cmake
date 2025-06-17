31dfa2ef4fb265dfaabfd570e86092213a9b652a
2023051604034XiangZaiShun <2142533685@qq.com>
2025-06-17 15:54:11 +0800

    delete build and .user

--- build/Desktop_Qt_6_9_1-Debug/.qt/QtDeployTargets.cmake
+++
@@ -1,2 +0,0 @@
-set(__QT_DEPLOY_TARGET_appChess_FILE /run/Chess/build/Desktop_Qt_6_9_1-Debug/appChess)
-set(__QT_DEPLOY_TARGET_appChess_TYPE EXECUTABLE)
--- build/Desktop_Qt_6_9_1-Debug/.qt/appChess_qml.cmake
+++
@@ -1,9 +0,0 @@
-
-set(target "appChess")
-set(working_dir "/run/Chess")
-set(src_and_dest_list
-    "/run/Chess/Main.qml"
-    "/run/Chess/build/Desktop_Qt_6_9_1-Debug/Chess/Main.qml"
-
-)
-set(timestamp_file "/run/Chess/build/Desktop_Qt_6_9_1-Debug/.qt/appChess_qml.txt")
